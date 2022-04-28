337c8029c4750384cb655e54daeec55b6970ceff
Aftabv1 <aftabrehan52422@gmail.com>
2022-04-28 10:04:09 +0500

    Add Basic Functionality

---
+++ script.js
@@ -0,0 +1,22 @@
+const countToDate = new Date().setHours(new Date().getHours() + 24);
+let previousTimeBetweenDates;
+setInterval(() => {
+  const currentDate = new Date();
+  const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
+  flipAllCards(timeBetweenDates);
+
+  previousTimeBetweenDates = timeBetweenDates;
+}, 250);
+
+function flipAllCards(time) {
+  const seconds = time % 60;
+  const minutes = Math.floor(time / 60) % 60;
+  const hours = Math.floor(time / 3600);
+
+  flip(document.querySelector("[data-hours-tens]"), Math.floor(hours / 10));
+  flip(document.querySelector("[data-hours-ones]"), hours % 10);
+  flip(document.querySelector("[data-minutes-tens]"), Math.floor(minutes / 10));
+  flip(document.querySelector("[data-minutes-ones]"), minutes % 10);
+  flip(document.querySelector("[data-seconds-tens]"), Math.floor(seconds / 10));
+  flip(document.querySelector("[data-seconds-ones]"), seconds % 10);
+}
